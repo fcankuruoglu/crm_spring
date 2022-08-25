6196e66afe29e1ef5e17d455027d79c613401e3b
the-undeadhood <ferruhcankuruoglu@gmail.com>
2022-08-25 12:47:21 +0300

    bugfix

--- src/main/java/com/example/crm/security/WebSecurityConfig.java
+++ src/main/java/com/example/crm/security/WebSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
@@ -17,8 +16,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import static java.lang.invoke.VarHandle.AccessMode.GET;
 
 @EnableWebSecurity
 @Configuration
@@ -39,7 +36,7 @@ public class WebSecurityConfig {
             "/swagger-ui/**",
             "/login"
     };
-    // rio-rio1234 USER-ADMIN  ferro-ferro1234 USER
+    // user-user admin-admin
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
@@ -54,48 +51,13 @@ public class WebSecurityConfig {
         http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
         http.authenticationManager(authenticationManager);
         http.authorizeRequests().antMatchers("/api/login", "/api/token/refresh").permitAll();
-        http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
-        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/user/save").hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests().antMatchers(AUTH_WHITELIST).permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/user/**","/api/users").hasAnyAuthority("ROLE_USER");
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/user/save", "/api/user/delete", "/api/company/**").hasAnyAuthority("ROLE_ADMIN");
+        http.httpBasic();
         http.authorizeRequests().anyRequest().authenticated();
-        //http.httpBasic(Customizer.withDefaults());
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-        //.authorizeHttpRequests((authz) -> authz
-        //.antMatchers(ADMIN_AUTH).hasAuthority("ROLE_ADMIN")
-        //.antMatchers(USER_AUTH).hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
-        //.antMatchers(AUTH_WHITELIST).permitAll())
         return http.build();
     }
-
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer() throws Exception{
-//        return (web) -> web.ignoring().antMatchers();
-//    }
-
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-//        return authenticationConfiguration.getAuthenticationManager();
-//    }
-//    @Bean
-//    public AuthenticationManager userDetailsService(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-//        return auth.build();
-//    }
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsService() {
-//        UserDetails user  = User
-//                .withUsername("user")
-//                .password(passwordEncoder.encode("password"))
-//                .roles("USER")
-//                .build();
-//        UserDetails admin = User
-//                .withUsername("admin")
-//                .password(passwordEncoder.encode("admin"))
-//                .roles("ADMIN")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
-
 
 }
